9fd4734b201f761ac8d3f17a287cd397cce3a5fe
Hantao-Hong <hh784@cornell.edu>
2024-12-09 20:08:32 -0500

    Finish the code for sentiment

--- project/run_sentiment.py
+++ project/run_sentiment.py
@@ -35,7 +35,8 @@ class Conv1d(minitorch.Module):
 
     def forward(self, input):
         # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        # raise NotImplementedError("Need to implement for Task 4.5")
+        return minitorch.conv1d(input, self.weights.value) + self.bias.value
 
 
 class CNNSentimentKim(minitorch.Module):
@@ -62,14 +63,31 @@ class CNNSentimentKim(minitorch.Module):
         super().__init__()
         self.feature_map_size = feature_map_size
         # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        # raise NotImplementedError("Need to implement for Task 4.5")
+        self.conv1 = Conv1d(embedding_size, feature_map_size, filter_sizes[0])
+        self.conv2 = Conv1d(embedding_size, feature_map_size, filter_sizes[1])
+        self.conv3 = Conv1d(embedding_size, feature_map_size, filter_sizes[2])
+        self.linear = Linear(feature_map_size, 1)
+        self.dropout_rate = dropout_rate
+
 
     def forward(self, embeddings):
         """
         embeddings tensor: [batch x sentence length x embedding dim]
         """
         # TODO: Implement for Task 4.5.
-        raise NotImplementedError("Need to implement for Task 4.5")
+        # raise NotImplementedError("Need to implement for Task 4.5")
+        c1 = self.conv1(embeddings).relu()
+        c2 = self.conv2(embeddings).relu()
+        c3 = self.conv3(embeddings).relu()
+
+        mid = (
+            minitorch.nn.max(x1, 2) + minitorch.nn.max(x2, 2) + minitorch.nn.max(x3, 2)
+        )
+        linear_out = self.linear(mid.view(mid.shape[0], min.shape[1]))
+        dropout_out = minitorch.nn.dropout(linear_out, self.dropout_rate)
+        return minitorch.nn.sigmoid(dropout_out)
+
 
 
 # Evaluation helper methods
