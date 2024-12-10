7a7786c1a744efb56bdfeaeb81941db407e1f69a
Hantao-Hong <hh784@cornell.edu>
2024-12-09 20:30:06 -0500

    Debug

--- project/run_sentiment.py
+++ project/run_sentiment.py
@@ -68,7 +68,7 @@ class CNNSentimentKim(minitorch.Module):
         self.conv2 = Conv1d(embedding_size, feature_map_size, filter_sizes[1])
         self.conv3 = Conv1d(embedding_size, feature_map_size, filter_sizes[2])
         self.linear = Linear(feature_map_size, 1)
-        self.dropout_rate = dropout_rate
+        self.dropout_rate = dropout
 
 
     def forward(self, embeddings):
